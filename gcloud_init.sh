693fc6abe62ed212a35b9d20324fcceb7cad2483
Alex Shemyakin <shemyakin@yoctocode.com>
2021-08-20 21:15:58 +0300

    update script

--- gcloud_init.sh
+++ gcloud_init.sh
@@ -12,16 +12,19 @@ PROJECT_UNIQUE_PREFIX=$(tr -dc a-z0-9 </dev/urandom | head -c 20 ; echo '')
 
 # create database
 gcloud sql instances create $SQL_INSTANCE_NAME \
---database-version=POSTGRES_13 \
---cpu=4 \
---memory=26GB \
---zone=$ZONE_NAME \
---root-password=$SQL_ROOT_PASSWORD \
---database-flags=cloudsql.iam_authentication=on
+    --database-version=POSTGRES_13 \
+    --cpu=4 \
+    --memory=26GB \
+    --zone=$ZONE_NAME \
+    --root-password=$SQL_ROOT_PASSWORD \
+    --database-flags=cloudsql.iam_authentication=on
 
 # restore database from backup
 gsutil mb gs://$PROJECT_UNIQUE_PREFIX-sqlbackup
 gsutil cp ./backup.sql.gz gs://$PROJECT_UNIQUE_PREFIX-sqlbackup
+IAM_SQL_ACCOUNT_FULL_NAME=$(gcloud sql instances describe $SQL_INSTANCE_NAME --format="value(serviceAccountEmailAddress)")
+gsutil acl ch -u $IAM_SQL_ACCOUNT_FULL_NAME:R gs://$PROJECT_UNIQUE_PREFIX-sqlbackup;
+gsutil acl ch -u $IAM_SQL_ACCOUNT_FULL_NAME:R gs://$PROJECT_UNIQUE_PREFIX-sqlbackup/backup.sql.gz;
 gcloud sql import sql $SQL_INSTANCE_NAME gs://$PROJECT_UNIQUE_PREFIX-sqlbackup/backup.sql.gz --database=$SQL_DATABASE_NAME
 
 # get compute service account
@@ -30,5 +33,5 @@ IAM_COMPUTE_ACCOUNT=$(echo "$IAM_COMPUTE_ACCOUNT_FULL_NAME" | sed "s/.gserviceac
 
 # add IAM user to sql database
 gcloud sql users create $IAM_COMPUTE_ACCOUNT \
---instance=$SQL_INSTANCE_NAME \
---type=cloud_iam_service_account
+    --instance=$SQL_INSTANCE_NAME \
+    --type=cloud_iam_service_account
