bb6eaae2877cbac8d7b934aa4950d058af03adfb
Alex Shemyakin <shemyakin@yoctocode.com>
2021-08-23 18:25:55 +0300

    Update script

--- gcloud_init.sh
+++ gcloud_init.sh
@@ -1,5 +1,8 @@
+echo "Enabling compute.googleapis.com"
 gcloud services enable compute.googleapis.com 
+echo "Enabling dns.googleapis.com "
 gcloud services enable dns.googleapis.com 
+echo "Enabling sqladmin.googleapis.com"
 gcloud services enable sqladmin.googleapis.com
 
 # set env variables
@@ -10,6 +13,7 @@ SQL_DATABASE_NAME=postgres
 SQL_ROOT_PASSWORD=example
 PROJECT_UNIQUE_PREFIX=$(tr -dc a-z0-9 </dev/urandom | head -c 20 ; echo '')
 
+echo "Creating Cloud SQL database"
 # create database
 gcloud sql instances create $SQL_INSTANCE_NAME \
     --database-version=POSTGRES_13 \
@@ -19,14 +23,17 @@ gcloud sql instances create $SQL_INSTANCE_NAME \
     --root-password=$SQL_ROOT_PASSWORD \
     --database-flags=cloudsql.iam_authentication=on
 
+echo "Restoring DB from the backup"
 # restore database from backup
 gsutil mb gs://$PROJECT_UNIQUE_PREFIX-sqlbackup
-gsutil cp ./backup.sql.gz gs://$PROJECT_UNIQUE_PREFIX-sqlbackup
 IAM_SQL_ACCOUNT_FULL_NAME=$(gcloud sql instances describe $SQL_INSTANCE_NAME --format="value(serviceAccountEmailAddress)")
 gsutil acl ch -u $IAM_SQL_ACCOUNT_FULL_NAME:R gs://$PROJECT_UNIQUE_PREFIX-sqlbackup;
 gsutil acl ch -u $IAM_SQL_ACCOUNT_FULL_NAME:R gs://$PROJECT_UNIQUE_PREFIX-sqlbackup/backup.sql.gz;
-gcloud sql import sql $SQL_INSTANCE_NAME gs://$PROJECT_UNIQUE_PREFIX-sqlbackup/backup.sql.gz --database=$SQL_DATABASE_NAME
+gsutil cp ./backup.sql.gz gs://$PROJECT_UNIQUE_PREFIX-sqlbackup
+gcloud sql import sql $SQL_INSTANCE_NAME gs://$PROJECT_UNIQUE_PREFIX-sqlbackup/backup.sql.gz --database=$SQL_DATABASE_NAME -q
+gsutil rm -r gs://$PROJECT_UNIQUE_PREFIX-sqlbackup
 
+echo "Adding IAM user to the SQL instance"
 # get compute service account
 IAM_COMPUTE_ACCOUNT_FULL_NAME=$(gcloud iam service-accounts list --format="value(email)" --filter=displayName:"Compute Engine default service account")
 IAM_COMPUTE_ACCOUNT=$(echo "$IAM_COMPUTE_ACCOUNT_FULL_NAME" | sed "s/.gserviceaccount.com//g")
@@ -35,3 +42,8 @@ IAM_COMPUTE_ACCOUNT=$(echo "$IAM_COMPUTE_ACCOUNT_FULL_NAME" | sed "s/.gserviceac
 gcloud sql users create $IAM_COMPUTE_ACCOUNT \
     --instance=$SQL_INSTANCE_NAME \
     --type=cloud_iam_service_account
+
+echo "SQL Password is: $SQL_ROOT_PASSWORD"
+echo "Execute command to grant permissions: grant SELECT, INSERT, UPDATE, DELETE on WINELIST to \"$IAM_COMPUTE_ACCOUNT\";"
+gcloud sql connect $SQL_INSTANCE_NAME --user=postgres
+ 
